b10f2ea22fa6cc1e9183b29bb6118ea4a7374958
Flavia Lopez <flavia.023.fl@gmail.com>
2025-04-04 09:02:18 -0300

    practicando

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//concatenacion de caracteres//
+
+let nombre = "Flavia";
+let apellido = "Lopez";
+
+console.log ("El nombre de usuario es: " + nombre + "Flavia Lopez " + apellido);
+
+//Comparacion de variables//
+ let numero = 36;
+
+ console.log (" ")
